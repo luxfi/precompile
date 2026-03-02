09fd7e6b38f02917a969f0584a5250ddbdd2eaa6
Hanzo Dev <dev@hanzo.ai>
2026-03-02 20:32:51 +0000

    refactor: migrate precompile modules to registry pattern
    Replace singleton module structs with modules.RegisterModule init()
    pattern for ecies, hpke, mldsa, mlkem, ring, slhdsa. Add proper
    Configurator interface with Config types for each module.

--- pqcrypto/module.go
+++ pqcrypto/module.go
@@ -1,7 +1,7 @@
 // Copyright (C) 2025, Lux Industries Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-package slhdsa
+package pqcrypto
 
 import (
 	"github.com/luxfi/precompile/contract"
@@ -11,15 +11,13 @@ import (
 
 var _ contract.Configurator = &configurator{}
 
-const ConfigKey = "slhdsaVerify"
-
 type configurator struct{}
 
 func init() {
 	if err := modules.RegisterModule(modules.Module{
 		ConfigKey:    ConfigKey,
-		Address:      ContractSLHDSAVerifyAddress,
-		Contract:     SLHDSAVerifyPrecompile,
+		Address:      ContractAddress,
+		Contract:     PQCryptoPrecompile,
 		Configurator: &configurator{},
 	}); err != nil {
 		panic(err)
@@ -36,19 +34,3 @@ func (*configurator) Configure(
 ) error {
 	return nil
 }
-
-type Config struct {
-	Upgrade precompileconfig.Upgrade `json:"upgrade,omitempty"`
-}
-
-func (c *Config) Key() string        { return ConfigKey }
-func (c *Config) Timestamp() *uint64 { return c.Upgrade.Timestamp() }
-func (c *Config) IsDisabled() bool   { return c.Upgrade.Disable }
-func (c *Config) Equal(cfg precompileconfig.Config) bool {
-	other, ok := cfg.(*Config)
-	if !ok {
-		return false
-	}
-	return c.Upgrade.Equal(&other.Upgrade)
-}
-func (c *Config) Verify(precompileconfig.ChainConfig) error { return nil }
